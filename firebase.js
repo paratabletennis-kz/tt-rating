43f7f5dc9980ba534200613f35ac55fdfd6d1086
paratabletennis-kz <zhaneka.kanatbayeva@gmail.com>
2026-08-01 19:38:03 +0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,4 +1,14 @@
-// Firebase будет подключен следующим шагом
+// firebase.js
+
+
+import { initializeApp } 
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+
+import { getFirestore } 
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+
 
 const firebaseConfig = {
 
@@ -7,9 +17,20 @@ apiKey: "ВАШ_API_KEY",
 
 authDomain: "ВАШ_PROJECT.firebaseapp.com",
 
-projectId: "ВАШ_PROJECT_ID"
+projectId: "ВАШ_PROJECT_ID",
+
+storageBucket: "ВАШ_STORAGE.appspot.com",
+
+messagingSenderId: "ВАШ_ID",
+
+appId: "ВАШ_APP_ID"
 
 
 };
 
 
+
+const app = initializeApp(firebaseConfig);
+
+
+export const db = getFirestore(app);
