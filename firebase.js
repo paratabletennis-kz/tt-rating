7ea069c0d6b4caa79d543b1e13f4075a5fb577fa
paratabletennis-kz <zhaneka.kanatbayeva@gmail.com>
2026-08-01 19:34:34 +0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,15 @@
+// Firebase будет подключен следующим шагом
+
+const firebaseConfig = {
+
+
+apiKey: "ВАШ_API_KEY",
+
+authDomain: "ВАШ_PROJECT.firebaseapp.com",
+
+projectId: "ВАШ_PROJECT_ID"
+
+
+};
+
 
